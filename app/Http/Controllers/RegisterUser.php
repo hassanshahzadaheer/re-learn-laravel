57fcdb8bde30a7aa2f66a440c1fc29782420fe97
aheer <hassanshahzadaheer@gmail.com>
2024-06-24 19:21:31 +0500

    fix register router issue

---
+++ app/Http/Controllers/RegisterUser.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterUser extends Controller
+{
+    public function index() {
+        return "updated";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\RegisterUser;
 
 Route::view('/', 'pages.home');
 Route::view('/contact', 'pages.contact');
 Route::resource('jobs',JobController::class);
 
 
-
+Route::get('/register', [RegisterUser::class,'index']);
 
 
 
