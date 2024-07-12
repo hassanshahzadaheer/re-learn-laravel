dd550743af8136fc068393f8a1ba17f5d405cfbc
aheer <hassanshahzadaheer@gmail.com>
2024-07-12 08:43:47 +0500

    improve home page design and add few card items

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -47,13 +47,6 @@
         </nav>
     </div>
 
-
-    <header class="bg-black">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
-            <h1 class="text-3xl font-bold tracking-tight ">{{ $heading }}</h1>
-        </div>
-    </header>
-
     <main class="mx-auto max-w-9x6 py-6 sm:px-6 lg:px-8">
         {{ $slot }}
     </main>
