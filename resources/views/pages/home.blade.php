e9bb93dab8b39dfe21ee2232d30bde26630ce2ed
aheer <hassanshahzadaheer@gmail.com>
2024-07-16 17:11:21 +0500

    desing completed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,15 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+            <form action="#" class="mt-6" >
+                <input class="rounded-xl px-4 py-4 w-full max-w-xl bg-white/5" type="text" id="search" name="search" placeholder="Full time developer">
+            </form>
+
+        </section>
+
+        <section class="pt-10">
             <x-section-heading> Feature Jobs </x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
                 <x-job-card-small />
